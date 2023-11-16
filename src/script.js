d75535e5e3627f22e140c5c5066e10c4b23449a1
antoine lecorre <antoinelecorre56@gmail.com>
2023-11-16 09:52:11 +0100

    suppression du axes helper

--- src/script.js
+++ src/script.js
@@ -15,8 +15,7 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+
 
 
 const flaqueMat = new THREE.ShaderMaterial({
